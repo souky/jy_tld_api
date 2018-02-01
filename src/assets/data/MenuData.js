798c790b3ecc07a333dd1e58916121a1b8e191d1
mswen <281939833@qq.com>
2018-02-01 16:10:55 +0800

    menu

---
+++ src/assets/data/MenuData.js
@@ -0,0 +1,51 @@
+/**
+ * @description 主页数据
+ */
+
+export default {
+	Attributes:[
+		{
+			parameter:'id',
+			explain:'原有属性',
+			type:'String',
+			chioceVal:'-',
+			defaultVal:'-'
+		},
+		{
+			parameter:'class',
+			explain:'原有属性',
+			type:'String',
+			chioceVal:'-',
+			defaultVal:'-'
+		},
+		{
+			parameter:'name',
+			explain:'原有属性',
+			type:'String',
+			chioceVal:'-',
+			defaultVal:'-'
+		},
+		{
+			parameter:'menu-data',
+			explain:'数据',
+			type:'Array',
+			chioceVal:'-',
+			defaultVal:'-'
+		},
+		{
+			parameter:'menu-open',
+			explain:'获取每一行keyClick得值',
+			type:'function(obj)',
+			chioceVal:'-',
+			defaultVal:'-'
+		},
+		
+//		{
+//			parameter:'',
+//			explain:'',
+//			type:'',
+//			chioceVal:'',
+//			defaultVal:''
+//		},
+	]
+}
---
+++ src/components/Menu.vue
@@ -0,0 +1,38 @@
+<template>
+	<div id="menu">
+		<div class="common_title">
+			Basics code
+		</div>
+		<div class="code_block">
+			#jy:menu % #/jy:menu%
+		</div>
+		<div class="common_title">
+			Menu Attributes
+		</div>
+		<el-table :data="menuData" style="width:90%;margin:auto;">
+			<el-table-column prop="parameter" label="参数" width='180px'></el-table-column>
+			<el-table-column prop="explain" label="说明"></el-table-column>
+			<el-table-column prop="type" label="类型" width='200px'></el-table-column>
+			<el-table-column prop="chioceVal" label="可选值" width='160px'></el-table-column>
+			<el-table-column prop="defaultVal" label="默认值" width='160px'></el-table-column>
+		</el-table>
+		
+	</div>
+</template>
+
+<script>
+import MenuData from '../assets/data/MenuData'
+export default {
+	data() {
+	  	return {
+	  		menuData:MenuData.Attributes,
+		}
+	},
+	mounted:function(){
+		this.initCodeBlock();
+	}
+}
+</script>
+
+<style>
+</style>
